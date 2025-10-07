b06a2f68c2b53ca2ec51297d5224a21a710c902c
Tanuka Sengupta <tanuka0202@gmail.com>
2025-10-07 16:34:39 +0530

    Update accessories.js

--- accessories.js
+++ accessories.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("handoverForm");
 
-  const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwMC3no_08ZawTanj2ZmvSFgFAmhNIqv2fNcTdx1BtkJjS1ex7b5i9Uay4IgPlL4-Kdcg/exec";
+  const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwPXguPTxpBljGq8bq6_5KCrHmMht8RuDokT42CdsK2CS-b2QkpPCyrWGmKAecsuSYvAw/exec";
 
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
@@ -41,3 +41,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
