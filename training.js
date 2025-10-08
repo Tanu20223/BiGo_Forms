e26e1185a8c6dae27a34c07d730d838a4e55d8a9
Tanuka Sengupta <tanuka0202@gmail.com>
2025-10-08 12:40:27 +0530

    Update training.js

--- training.js
+++ training.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const previousJobGroup = document.getElementById("previousJobGroup");
   const submitBtn = form.querySelector('button[type="submit"]'); // ✅ define submit button
 
-  const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwMC3no_08ZawTanj2ZmvSFgFAmhNIqv2fNcTdx1BtkJjS1ex7b5i9Uay4IgPlL4-Kdcg/exec"; // 🔹 replace
+  const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbx4yXD5S23Suxb8IQGPRkOS9NGB1ggv5sJtbKvTmz_cLiA3ooAO04iMc4vrSVH9iCnVSg/exec"; // 🔹 replace
 
   // Show/hide Previous Job
   trainingTypeSelect.addEventListener("change", () => {
@@ -52,6 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
   
+
 
 
 
