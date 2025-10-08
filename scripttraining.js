245e6c2dae3a3a59604bb9230dcb5ddc794534eb
Tanuka Sengupta <tanuka0202@gmail.com>
2025-10-08 17:04:21 +0530

    Update scripttraining.js

--- scripttraining.js
+++ scripttraining.js
@@ -48,9 +48,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const data = {
       formType: "training",
-      phone: currentPhone,
       visitorName: document.getElementById("visitorName").value,
       email: document.getElementById("email").value,
+      phone: currentPhone,
       altNumber: document.getElementById("altNumber").value,
       area: document.getElementById("area").value,
       reference: document.getElementById("reference").value,
@@ -105,3 +105,4 @@ followupForm.addEventListener("submit", async (e) => {
     }
   }
 });
+
